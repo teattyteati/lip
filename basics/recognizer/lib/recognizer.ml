9482375ab2a5f2aab662e40b2df018fcd8024b4d
teattyteati <teatty.teati@gmail.com>
2024-11-05 12:34:08 +0100

    primi 2 linguaggi

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,6 +1,15 @@
-let lang1 _ = failwith ""
+let rec lang1 w = match w with
+                  [0] -> true
+                | [1] -> true
+                | [] -> false
+                | h::w -> if h = 0 then lang1 w else if h = 1 then lang1 w else false;;
 
-let lang2 _ = failwith ""
+let rec lang2 w = match w with
+                  [] -> false
+                | [0] -> true
+                | [1] -> true
+                | h1::h2::w -> if h1 = 1 && h2 = 0 || h1 = 0 && h2 = 0 then false else lang2 (h2::w)
+                | _ -> false;;
 
 let lang3 _ = failwith ""
 
